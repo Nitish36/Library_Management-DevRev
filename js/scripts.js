05fe57da1eab0318246fca40b0e73ec2d454aad5
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2022-11-11 21:01:34 +0530

    Final Commits

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#mySearch").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#filt tr").filter(function() {
+            $(this).toggle($(this).text()
+            .toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+$(document).ready(function () {   
+   
+    $(function () {
+        $('#mySearch').datepicker({
+            format: 'DD-MM-YYYY',
+        });
+      });
+
+    $('#mySearch').on('dp.change', function (e) {
+     var rex = new RegExp($(this).val(), "i");
+    $("#books tr").hide();
+    $("#books tr").filter(function () {
+        return rex.test($(this).text());
+    }).show();
+    $(".noResults").hide();
+    if ($("#books tr:visible").length == 0) {
+        $(".noResults").show();
+    }
+    });  
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        
+      // Select all the rows in the table
+      // and get the count of the selected elements
+      var rowCount = $("#filt tr").length;
+      alert(rowCount); 
+    });
+  });
